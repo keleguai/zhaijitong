ee539c85b1a6de94b4b733373bdd367b9d242278
Your Name <you@example.com>
2019-06-11 16:09:14 +0800

    配置redis

--- src/test/java/cn/edu/neu/School_Jobs/SchoolJobsApplicationTests.java
+++ src/test/java/cn/edu/neu/School_Jobs/SchoolJobsApplicationTests.java
@@ -7,18 +7,25 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class SchoolJobsApplicationTests {
     @Autowired
     private UserService userService;
     @Test
     public void contextLoads() {
-        System.out.println(userService.selectAllByPhone("18689952277"));
+        Jedis jedis = new Jedis("localhost");
+        System.out.println(jedis.getClient().getPort());
+        System.out.println("连接本地的Redis服务器成功");
+        //查看服务是否运行
+        System.out.println("服务正在运行：" + jedis.ping());
+
+//        System.out.println(userService.selectAllByPhone("18689952277"));
     }
 
     @Test
